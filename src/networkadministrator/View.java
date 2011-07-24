9f5563797056b18e7674c5f1c69a4f278e6075d8
Isaac Tepper <Isaac356@live.com>
2011-07-23 23:37:04 -0400

    Added license stubs

--- src/networkadministrator/View.java
+++ src/networkadministrator/View.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Isaac Tepper and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Isaac Tepper - initial implementation
+ ******************************************************************************/
+
 package networkadministrator;
 
 import org.eclipse.jface.dialogs.MessageDialog;
