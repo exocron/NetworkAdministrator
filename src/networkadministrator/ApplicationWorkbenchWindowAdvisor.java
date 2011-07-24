85e7adc4a2a061a59e6b0a28487aa9e69fbec6ec
Isaac Tepper <Isaac356@live.com>
2011-07-23 21:48:30 -0400

    Made window full-screen

--- src/networkadministrator/ApplicationWorkbenchWindowAdvisor.java
+++ src/networkadministrator/ApplicationWorkbenchWindowAdvisor.java
@@ -1,5 +1,6 @@
 package networkadministrator;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
@@ -22,8 +23,14 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 	public void preWindowOpen() {
 		IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
 		configurer.setInitialSize(new Point(600, 400));
+		configurer.setShellStyle(SWT.NO_TRIM);
 		configurer.setShowCoolBar(true);
 		configurer.setShowStatusLine(false);
 		configurer.setTitle("Network Administrator");
+	}
+
+	@Override
+	public void postWindowOpen() {
+		getWindowConfigurer().getWindow().getShell().setMaximized(true);
 	}
 }
